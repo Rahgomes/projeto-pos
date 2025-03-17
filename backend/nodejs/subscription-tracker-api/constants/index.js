37cecac72b29564820784faf89fe12830a3aff99
root <root@DESKTOP-FGR8H93.>
2025-03-16 21:55:04 -0300

    backend: implementando constants para resolver MAGIC STRING/NUMBER no middleware de erros

---
+++ backend/nodejs/subscription-tracker-api/constants/index.js
@@ -0,0 +1,5 @@
+import HTTP_STATUS from "./httpStatusCodes.js";
+import ERROR_MESSAGES from "./messages.js";
+import MONGOOSE_ERRORS from "./mongooseErrors.js";
+
+export { HTTP_STATUS, ERROR_MESSAGES, MONGOOSE_ERRORS };
