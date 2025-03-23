b5577f1a59e77fc8db9d01eb5e14d661d10af122
root <root@DESKTOP-FGR8H93.>
2025-03-22 22:55:14 -0300

    backend: rotas de auth e middleware de autorização

--- backend/nodejs/subscription-tracker-api/middlewares/error.middleware.js
+++ backend/nodejs/subscription-tracker-api/middlewares/error.middleware.js
@@ -5,38 +5,45 @@ import {
   MONGOOSE_ERRORS,
 } from "../constants/index.js";
 
-const errorMiddleware = (err, _, res) => {
-  let error = new ErrorResponse(
-    err.message || ERROR_MESSAGES.SERVER_ERROR,
-    err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR
-  );
+const errorMiddleware = (err, _, res, next) => {
+  try {
+    let error =
+      err instanceof ErrorResponse
+        ? err
+        : new ErrorResponse(
+            err.message || ERROR_MESSAGES.SERVER_ERROR,
+            err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR
+          );
 
-  console.error(err);
+    console.error(err);
 
-  if (err.name === MONGOOSE_ERRORS.CAST_ERROR) {
-    error = new ErrorResponse(
-      ERROR_MESSAGES.RESOURCE_NOT_FOUND,
-      HTTP_STATUS.NOT_FOUND
-    );
+    if (err.name === MONGOOSE_ERRORS.CAST_ERROR) {
+      error = new ErrorResponse(
+        ERROR_MESSAGES.RESOURCE_NOT_FOUND,
+        HTTP_STATUS.NOT_FOUND
+      );
+    }
+
+    if (err.code === MONGOOSE_ERRORS.DUPLICATE_KEY) {
+      const field = Object.keys(err.keyValue)[0];
+      error = new ErrorResponse(
+        `${ERROR_MESSAGES.DUPLICATE_KEY}: ${field}`,
+        HTTP_STATUS.BAD_REQUEST
+      );
+    }
+
+    if (err.name === MONGOOSE_ERRORS.VALIDATION_ERROR) {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      error = new ErrorResponse(messages.join(", "), HTTP_STATUS.BAD_REQUEST);
+    }
+
+    res.status(error.statusCode).json({
+      success: false,
+      error: error.message,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  if (err.code === MONGOOSE_ERRORS.DUPLICATE_KEY) {
-    const field = Object.keys(err.keyValue)[0];
-    error = new ErrorResponse(
-      `${ERROR_MESSAGES.DUPLICATE_KEY}: ${field}`,
-      HTTP_STATUS.BAD_REQUEST
-    );
-  }
-
-  if (err.name === MONGOOSE_ERRORS.VALIDATION_ERROR) {
-    const messages = Object.values(err.errors).map((val) => val.message);
-    error = new ErrorResponse(messages.join(", "), HTTP_STATUS.BAD_REQUEST);
-  }
-
-  res.status(error.statusCode).json({
-    success: false,
-    error: error.message,
-  });
 };
 
 export default errorMiddleware;
--- backend/nodejs/subscription-tracker-api/utils/errorResponse.util.js
+++ backend/nodejs/subscription-tracker-api/utils/errorResponse.util.js
@@ -2,6 +2,8 @@ class ErrorResponse extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
+    this.name = "ErrorResponse";
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
