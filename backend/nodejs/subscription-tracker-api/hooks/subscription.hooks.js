b784747a4d8315b19c697686183714aca06b5009
root <root@DESKTOP-FGR8H93.>
2025-03-26 22:12:08 -0300

    backend: criando rotas de criação e obtenção de assinaturas por usuário

--- backend/nodejs/subscription-tracker-api/hooks/subscription.hooks.js
+++ backend/nodejs/subscription-tracker-api/hooks/subscription.hooks.js
@@ -1,7 +1,7 @@
 import {
   calculateRenewalDate,
   updateStatusIfExpired,
-} from "../utils/subscription.utils";
+} from "../utils/subscription.utils.js";
 
 const attachSubscriptionHooks = (schema) => {
   schema.pre("save", function (next) {
